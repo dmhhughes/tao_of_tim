893787b20a5fad5a671405229818e961a66353cc
Dylan Hughes <dylanhughes9@live.com>
2025-03-19 06:22:17 -0500

    updates for channel usage

--- R/send_telegram.R
+++ R/send_telegram.R
@@ -14,13 +14,13 @@ sample_show_link <- show_links_df |>
 # paste together markdown for message parse_mode
 telegram_message <- paste0(
   "Happy ", lubridate::wday(Sys.Date(), label = TRUE, abbr = FALSE), "! ", 
-  "Today's Tao te Tim comes from the episode, ", 
-  "[", sample_show_link$article, "]", "(", sample_show_link$urls, ")", 
+  "Today's Tao te Tim comes from the episode with ", 
+  "[", sample_show_link$article, "]", "(", sample_show_link$urls, ").", 
   " Here's the show link: ", 
   "[", sample_show_link$link_title, "]", "(", sample_show_link$link_url, ")")
 
 bot <- telegram.bot::Bot(token = Sys.getenv("R_TELEGRAM_BOT_TIM_TAO_BOT"))
 
 bot$sendMessage(
-  Sys.getenv("TELEGRAM_PERSONAL_ID"), 
+  Sys.getenv("TELEGRAM_CHANNEL_ID"), 
   text = telegram_message, parse_mode = "Markdown")
